6736e788dee4547bd650e25479320e282a2ce622
ManiackMike <105581753+ManiackMike@users.noreply.github.com>
2022-07-03 16:13:13 +0200

    a
    a

--- Blog/Blog_files/Blog.js
+++ Blog/Blog_files/Blog.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function photocastSubscribe()
-{photocastHelper("http://iphonenetwork.ml/iPhone_Network/Blog/rss.xml");}
+{photocastHelper("http://iPhone_Network/Blog/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Blog_files/BlogMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id6');performPostEffectsFixups()}
